4c6ec8368a8d82bf80fe0dda51f39dd1606305f8
Alexey Kalyakin <paxclub@yandex.ru>
2022-02-04 16:47:31 +0300

    update StatisticsServiceTest.java

--- src/test/java/ru/netology/stats/StatisticsServiceTest.java
+++ src/test/java/ru/netology/stats/StatisticsServiceTest.java
@@ -12,7 +12,16 @@ class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+        long actual = service.findMax(incomesInBillions);
 
+        assertEquals(expected, actual);
+    }
+    @Test
+    void ifFirstNumberIsLessThan() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
